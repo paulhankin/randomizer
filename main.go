5555483ac0d9e14c222295a0fc976268b90b3561
Paul Hankin <paul.hankin@pobox.com>
2024-02-06 11:57:25 +0100

    refresh back to click state

--- main.go
+++ main.go
@@ -30,9 +30,11 @@ type StateOne struct {
 }
 
 type State struct {
-	was, now  StateOne
-	clickTime time.Time
-	tloc      f32.Point
+	was, now    StateOne
+	clickTime   time.Time
+	tloc        f32.Point
+	refreshTime time.Time
+	needRefresh bool
 }
 
 func rgb(r, g, b int) color.NRGBA {
@@ -77,99 +79,117 @@ func colorLerp(c, d color.NRGBA, t float64) color.NRGBA {
 	return color.NRGBA{r, g, b, a}
 }
 
-const transitionTime = 500 * time.Millisecond
+const (
+	transitionTime  = 500 * time.Millisecond
+	refreshDuration = 2 * time.Second
+)
 
 func (s *State) run(w *app.Window) error {
 	th := material.NewTheme(gofont.Collection())
 	var ops op.Ops
+	const pingDuration = 500 * time.Millisecond
+	timer := time.NewTimer(pingDuration)
 	for {
-		e := <-w.Events()
-		switch e := e.(type) {
-		case system.DestroyEvent:
-			return e.Err
-		case system.FrameEvent:
-			gtx := layout.NewContext(&ops, e)
-			released := false
-			var ploc f32.Point
-			for _, e := range gtx.Events(clickHandler) {
-				if pe, ok := e.(pointer.Event); ok {
-					if time.Now().Sub(s.clickTime) >= transitionTime {
-						released = true
-						ploc = pe.Position
-					}
-				}
-			}
-			if released {
+		select {
+		case <-timer.C:
+			timer.Reset(pingDuration)
+			if s.needRefresh && time.Now().After(s.refreshTime) {
+				s.needRefresh = false
 				s.was = s.now
-				rnd := rand.Intn(100) + 1
-				col0 := rgb(255, 0, 0)
-				col1 := rgb(200, 200, 0)
-				col2 := rgb(0, 255, 0)
-				var bg color.NRGBA
-				if rnd <= 50 {
-					bg = colorLerp(col0, col1, float64(rnd)/50)
-				} else {
-					bg = colorLerp(col1, col2, float64(rnd-50)/50)
-				}
-				fg := rgb(255-int(bg.R), 255-int(bg.G), 255-int(bg.B))
-				fg = colorLerp(rgb(0, 0, 0), fg, 0.5)
-				s.now = StateOne{
-					bg:  bg,
-					fg:  fg,
-					msg: strconv.Itoa(rnd),
-				}
+				s.now = StateOne{bg: rgb(0, 0, 0), fg: rgb(255, 255, 255), msg: "click"}
 				s.clickTime = time.Now()
-				s.tloc = ploc
+				w.Invalidate()
 			}
-
-			t := math.Min(1.0, float64(time.Now().Sub(s.clickTime))/float64(transitionTime))
-			pointer.InputOp{Tag: clickHandler, Types: pointer.Release}.Add(gtx.Ops)
-			if t < 1 {
-				op.InvalidateOp{}.Add(gtx.Ops)
-			}
-			for i, st := range []StateOne{s.was, s.now, s.now} {
-				if t == 1 && i < 2 {
-					continue
-				}
-				var stack clip.Stack
-				bgDarken := 1.0
-				if i > 0 && t < 1 {
-					mx := float64(gtx.Constraints.Max.X)
-					my := float64(gtx.Constraints.Max.Y)
-					D := math.Max(mx, my)
-					tt := math.Pow(t, 5)
-					radius := float32(D * tt)
-					if i == 1 {
-						radius *= 1.5
-						bgDarken = 0.8
+		case e := <-w.Events():
+			switch e := e.(type) {
+			case system.DestroyEvent:
+				return e.Err
+			case system.FrameEvent:
+				gtx := layout.NewContext(&ops, e)
+				released := false
+				var ploc f32.Point
+				for _, e := range gtx.Events(clickHandler) {
+					if pe, ok := e.(pointer.Event); ok {
+						if time.Now().Sub(s.clickTime) >= transitionTime {
+							released = true
+							ploc = pe.Position
+						}
 					}
-					tx := s.tloc.X
-					ty := s.tloc.Y
-					emin := f32.Point{X: tx - radius, Y: ty - radius}
-					emax := f32.Point{X: tx + radius, Y: ty + radius}
-					stack = clip.Ellipse{Min: emin, Max: emax}.Push(gtx.Ops)
 				}
-				paint.ColorOp{Color: colorLerp(rgb(0, 0, 0), st.bg, bgDarken)}.Add(gtx.Ops)
-				paint.PaintOp{}.Add(gtx.Ops)
-				layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-					layout.Flexed(1.0, func(gtx layout.Context) layout.Dimensions {
-						return layout.Dimensions{Size: gtx.Constraints.Max}
-					}),
-					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						title := material.H1(th, st.msg)
-						title.Color = st.fg
-						title.Alignment = text.Middle
-						return title.Layout(gtx)
-					}),
-					layout.Flexed(1.0, func(gtx layout.Context) layout.Dimensions {
-						return layout.Dimensions{Size: gtx.Constraints.Max}
-					}),
-				)
-				if i > 0 && t < 1 {
-					stack.Pop()
+				if released {
+					s.was = s.now
+					rnd := rand.Intn(100) + 1
+					col0 := rgb(255, 0, 0)
+					col1 := rgb(200, 200, 0)
+					col2 := rgb(0, 255, 0)
+					var bg color.NRGBA
+					if rnd <= 50 {
+						bg = colorLerp(col0, col1, float64(rnd)/50)
+					} else {
+						bg = colorLerp(col1, col2, float64(rnd-50)/50)
+					}
+					fg := rgb(255-int(bg.R), 255-int(bg.G), 255-int(bg.B))
+					fg = colorLerp(rgb(0, 0, 0), fg, 0.5)
+					s.now = StateOne{
+						bg:  bg,
+						fg:  fg,
+						msg: strconv.Itoa(rnd),
+					}
+					s.clickTime = time.Now()
+					s.tloc = ploc
+					s.refreshTime = s.clickTime.Add(refreshDuration)
+					s.needRefresh = true
 				}
+
+				t := math.Min(1.0, float64(time.Now().Sub(s.clickTime))/float64(transitionTime))
+				pointer.InputOp{Tag: clickHandler, Types: pointer.Release}.Add(gtx.Ops)
+				if t < 1 {
+					op.InvalidateOp{}.Add(gtx.Ops)
+				}
+				for i, st := range []StateOne{s.was, s.now, s.now} {
+					if t == 1 && i < 2 {
+						continue
+					}
+					var stack clip.Stack
+					bgDarken := 1.0
+					if i > 0 && t < 1 {
+						mx := float64(gtx.Constraints.Max.X)
+						my := float64(gtx.Constraints.Max.Y)
+						D := math.Max(mx, my)
+						tt := math.Pow(t, 5)
+						radius := float32(D * tt)
+						if i == 1 {
+							radius *= 1.5
+							bgDarken = 0.8
+						}
+						tx := s.tloc.X
+						ty := s.tloc.Y
+						emin := f32.Point{X: tx - radius, Y: ty - radius}
+						emax := f32.Point{X: tx + radius, Y: ty + radius}
+						stack = clip.Ellipse{Min: emin, Max: emax}.Push(gtx.Ops)
+					}
+					paint.ColorOp{Color: colorLerp(rgb(0, 0, 0), st.bg, bgDarken)}.Add(gtx.Ops)
+					paint.PaintOp{}.Add(gtx.Ops)
+					layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+						layout.Flexed(1.0, func(gtx layout.Context) layout.Dimensions {
+							return layout.Dimensions{Size: gtx.Constraints.Max}
+						}),
+						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+							title := material.H1(th, st.msg)
+							title.Color = st.fg
+							title.Alignment = text.Middle
+							return title.Layout(gtx)
+						}),
+						layout.Flexed(1.0, func(gtx layout.Context) layout.Dimensions {
+							return layout.Dimensions{Size: gtx.Constraints.Max}
+						}),
+					)
+					if i > 0 && t < 1 {
+						stack.Pop()
+					}
+				}
+				e.Frame(gtx.Ops)
 			}
-			e.Frame(gtx.Ops)
 		}
 	}
 }
